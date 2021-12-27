a6bb0f21317e6ad9209ea5ef48d9cc5dd0f679ec
nac19 <74272103+nac19@users.noreply.github.com>
2021-12-27 03:40:52 +0300

    Merhaba ben Volkan Konak

---
+++ merhababenvolkankonak.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\n\n\n           Merhaba\n\n");
+        System.out.print("\n                              ``````.''`                                   ");
+        System.out.print("\n                            .=in}c?xhbdWVr_                                ");
+        System.out.print("\n                          '^VUwtn]v]nnzaW0$mr                              ");
+        System.out.print("\n                        `\"}UVx(?r^<<<*?ilJKd$U\"                          ");
+        System.out.print("\n                       -:iV]v|?*^<~~>*r?|x}nsGb~                           ");
+        System.out.print("\n                      .:v}x]]]?*^<~>^^*rvx}cVcVO*                          ");
+        System.out.print("\n                     `-=i]]}]i?*^^^<^**?|v]nyn]d8=                         ");
+        System.out.print("\n                     -,^}}xixi(*<~~~~>^rvcsXtzl6#Z.                        ");
+        System.out.print("\n                     -=}liiV6ZUmmV|**izSZPmbanj6##]                        ");
+        System.out.print("\n                     '^j]vv}jaqZPt]^^zVVjqZPVlVKg#H'                       ");
+        System.out.print("\n                     'rUV](r|i}]v??**c(*?v(r?}wP6Qh-                       ");
+        System.out.print("\n                     ,?Uj}vr^<!!~*r>~i]r!=!riVXm$Dr`                       ");
+        System.out.print("\n                     'riiii?r^^(v|r<!inz(^^|nVV2p]_                        ");
+        System.out.print("\n                      ,(>(xrrrxVyKSP5RgOj]](]njj?-                         ");
+        System.out.print("\n                      `*~~li**XPKUXVyaS6dSml}wh?`                          ");
+        System.out.print("\n                       -=:xn(*l(ix]]nnn2V}n]nU}.                           ");
+        System.out.print("\n                       `,!*}nJ}ilnV}]]nVV}VttU^                            ");
+        System.out.print("\n                      `,]z=?VqKjlvv]((]}lwdHhq]_                           ");
+        System.out.print("\n                '!?}wb8B#D<nmqZmKUjjVnVwKWSjt5##8Pn|!`                     ");
+        System.out.print("\n          `=(V5g#@@@@###@#i^tozyehmPqHPUhynVlvb#@@@@@#Q6Un|~-`             ");
+        System.out.print("\n     `>lSg#@@@@@@@@@@#@@@@ZvV}v?v]lnnVu]]]ul*^*$@@@@@@@@@@@@@#QX-          ");
+        System.out.print("\n    rB@##@@@@@@@@@@@@@@@@@@ZW]V?^?vviv??}J}rrr>v#@@@@@@@@@@@@@@@8!         ");
+        System.out.print("\n   'd#@@@@@@@@@@@#@@@@@@@@@#pv}n?r(??rr}cr>~!=!e#@@@@@@@@@@@@@@@@#~        ");
+        System.out.print("\n   v@@@@@@@@@@@@@#@@@@@@@@@@@P?r|r*^<^||!~!=::z@@@@@@@@@@@@@@@@@@@B=       ");
+        System.out.print("\n  `b@@@@@@@@@#@@@@#@@@@@@@@@@#irvvr^^r^=<^!!*E@@@@@@@@@@@@@@@@@@@@@$'      ");
+        System.out.print("\n  !#@@@@@@@@@@@@@@@@@@@@@@@@@@p<!?(?r*=!<rub#@@@@@@@@@@@@@@@@@@@@@@@V      ");
+        System.out.print("\n  r@@@@@@@@@@@@@@@@@@@@@@@@@@@#z::<(i*:^Z#@@@@@@@@@@@@@@@@@@@@@@@@@@#,     ");
+        System.out.print("\n 'q@@@@@@@@@@@@#@@@#@@@@@@@@@@@B?:,!?~rq@@@@@@@@@@@@@@@@@@@@@@@@@@@@@]     ");
+        System.out.print("\n -6@@@@@@@@@@@@@@@@#@@@@@@@@@@@@gr!:>nx8@@@@@@@@@@@@@@@@#@@@@@@@@@@@@#~    ");
+        System.out.print("\n  m@@@@@@@@@@@@@@@@#@@@@@@@@@@@@@t~~i}x#@@@@@@@@@@@@@@@#@@@@@@@@@@@@@@}    ");
+        System.out.print("\n  X@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@g?(v<2@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@E`   ");
+        System.out.print("\n  }@@@@@@@@@@@@@@@@##@@@@@@@@@@@@#cv^=Z@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(   ");
+        System.out.print("\n  ^#@@@@@@@@@@@@@Q#####@@@@@@@@@@#R?~~g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@m   ");
+        System.out.print("\n  =#@@@@@@@@@@@@@8#######@@@@@@@@##u~*B@@@@@@@@@@#8dUXZB@@@@@@@@@@@@@@@#,  ");
+        System.out.print("\n  'N@@@@@@@@@@@@@@@@@@@@@@@@@@@###@#QB##@@#####Klr^<~!~}####@@@@@@@@@@@#:  ");
+        System.out.print("\n   v#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#@@@@@@@@@@5=:~^*r?}##@##@@@@@@@@@@@?  ");
+        System.out.print("\n   ,B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##@@@@@@@@@@R!==r?x]}g#@@@@@@@@@@@@@@i  ");
+        System.out.print("\n    2@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#@@@@@@@@@@@@g^~~]a0##@@@@@@@@@@@@@@@@h  ");
+        System.out.print("\n    '6@@@@@@@@@@@@@@@@@@@@@@@@@@@#@@@@@@@@@@@@@$]ud#@@@@@@@@@@@@@@#@@@@@s  ");
+        System.out.print("\n     _0@@@@@@@@@@@@@@@@@@@@@@@@@@@#############B#@@@@@@@@@@@@@@@@@@@@@@@(  ");
+        System.out.print("\n      _R@@@@@@@#######@@@@@@@####B880gN8gQ######B#####@@@@@@@@@@@@@@@@@#!  ");
+        System.out.print("\n\n\n           Merhaba ben Volkan Konak\n\n\n\n");
+    }
+}
